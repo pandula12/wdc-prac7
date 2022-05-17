962a54a024da15af9b7c4772fb942d670e000290
pandula12 <pandulagajadeera09@gmail.com>
2022-05-18 01:22:24 +0930

    prac 7 task-1-2

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/brew', function(req, res,) {
+router.get('/brew', function(req, res) {
   var q = req.query.drink;
   if (q === "tea") {
     res.send("A delicious cup of tea!");
   }
   else if (q === "coffee") {
     res.sendStatus(418);
+  }
+  else {
+    res.sendStatus(400);
+  }
+});
+
+var count = 0;
+var messageArray = [];
+router.get('/pass-it-on', function(req, res) {
+  var q = req.query.message;
+  if (q) {
+    if (count == 0) {
+      res.send("first")
+    }
+    else {
+      res.send(messageArray[count-1]);
+    }
+    messageArray[count] = q;
+    count++;
   }
   else {
     res.sendStatus(400);
