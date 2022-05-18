dea2a90bfe90b9d8c19433ad45daa4da2ce3a2c7
pandula12 <pandulagajadeera09@gmail.com>
2022-05-19 03:28:18 +0930

    prac 7 task 1-4

--- public/javascripts/page.js
+++ public/javascripts/page.js
@@ -15,32 +15,31 @@ function recieve() {
         if (this.readyState == 4 && this.status == 200) {
             blogs = JSON.parse(this.responseText);
             blogs = blogs.reverse();
+            var e = document.getElementById("post-list");
+            var child = e.lastElementChild;
+            while (child) {
+                e.removeChild(child);
+                child = e.lastElementChild;
+            }
+            var blogsLength = blogs.length;
+            var temp;
+            var header;
+            var body;
+            for (i = 0; i < blogsLength; i++) {
+                temp = document.createElement('div');
+                header = document.createElement('h2');
+                body = document.createElement('p');
+                header.innerHTML = blogs[i].title;
+                body.innerHTML = blogs[i].content;
+                document.getElementById("post-list").appendChild(temp);
+                temp.appendChild(header);
+                temp.appendChild(body);
+            }
         }
     };
     xhttp.open("GET", "/users/getposts", true);
     xhttp.send();
-    post_blogs();
+
+
 }
 
-function post_blogs() {
-    var e = document.getElementById("post-list");
-    var child = e.lastElementChild;
-    while (child) {
-        e.removeChild(child);
-        child = e.lastElementChild;
-    }
-    var blogsLength = blogs.length;
-    var temp;
-    var header;
-    var body;
-    for (i = 0; i < blogsLength; i++) {
-        temp = document.createElement('div');
-        header = document.createElement('h2');
-        body = document.createElement('p');
-        header.innerHTML = blogs[i].title;
-        body.innerHTML = blogs[i].content;
-        document.getElementById("post-list").appendChild(temp);
-        temp.appendChild(header);
-        temp.appendChild(body);
-    }
-}
