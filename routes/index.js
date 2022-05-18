7e7d714b0fb6eeb993e20bd168d6848167494b04
pandula12 <pandulagajadeera09@gmail.com>
2022-05-18 20:27:53 +0930

    prac 7 task 1-3

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/brew', function(req, res) {
+router.get('/brew', function (req, res) {
   var q = req.query.drink;
   if (q === "tea") {
     res.send("A delicious cup of tea!");
@@ -21,14 +22,14 @@ router.get('/brew', function(req, res) {
 
 var count = 0;
 var messageArray = [];
-router.post('/pass-it-on', function(req, res) {
+router.post('/pass-it-on', function (req, res) {
   var q = req.body.message;
   if (q) {
     if (count == 0) {
       res.send("first")
     }
     else {
-      res.send(messageArray[count-1]);
+      res.send(messageArray[count - 1]);
     }
     messageArray[count] = q;
     count++;
@@ -36,6 +37,16 @@ router.post('/pass-it-on', function(req, res) {
   else {
     res.sendStatus(400);
   }
+});
+
+router.post('/combine', function (req, res) {
+  var linesArray = req.body.lines;
+  var string = "";
+  var suf = req.body.suffix;
+  linesArray.forEach(function (el, index) {
+    string = string + el + suf + "\n";
+  });
+  res.send(string);
 });
 
 module.exports = router;
