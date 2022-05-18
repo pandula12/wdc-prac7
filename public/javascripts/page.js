f331e432c1d9940a80cfc170fda5b1511d520895
pandula12 <pandulagajadeera09@gmail.com>
2022-05-19 03:24:09 +0930

    prac 7 task 1-4

---
+++ public/javascripts/page.js
@@ -0,0 +1,46 @@
+function send() {
+    var title = document.getElementById("blogTitle").value;
+    var content = document.getElementById("blogContent").value;
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.open("POST", "/users/addpost", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify({ title: title, content: content }));
+}
+var blogs = []
+function recieve() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            blogs = JSON.parse(this.responseText);
+            blogs = blogs.reverse();
+        }
+    };
+    xhttp.open("GET", "/users/getposts", true);
+    xhttp.send();
+    post_blogs();
+}
+
+function post_blogs() {
+    var e = document.getElementById("post-list");
+    var child = e.lastElementChild;
+    while (child) {
+        e.removeChild(child);
+        child = e.lastElementChild;
+    }
+    var blogsLength = blogs.length;
+    var temp;
+    var header;
+    var body;
+    for (i = 0; i < blogsLength; i++) {
+        temp = document.createElement('div');
+        header = document.createElement('h2');
+        body = document.createElement('p');
+        header.innerHTML = blogs[i].title;
+        body.innerHTML = blogs[i].content;
+        document.getElementById("post-list").appendChild(temp);
+        temp.appendChild(header);
+        temp.appendChild(body);
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+var blogList = [];
+var blogListReverse = [];
+var count = 0;
+
+router.post('/addpost', function(req, res) {
+  if (req.body.hasOwnProperty("title") && req.body.hasOwnProperty("content")) {
+    blogList.push(req.body);
+  }
+  count++;
+  res.send();
+}); 
+
+
+router.get('/getposts', function(req, res) {
+  res.send(blogList);
+});
+
 module.exports = router;
